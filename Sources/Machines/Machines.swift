6ff08d10547adb77e7df47e241d0a80afd844d73
Yury Korolev <yury.korolev@gmail.com>
2021-06-03 16:09:40 +0300

    Cleanup error handling

--- Sources/Machines/Machines.swift
+++ Sources/Machines/Machines.swift
@@ -110,13 +110,7 @@ public enum Machines {
     }
     
     public func ip() -> Promise<String, Error> {
-      client.run(command: "ip").stringFor(key: "ip").flatMap { ip in
-        if ip.isEmpty {
-          return .fail(.machineIsNotStarted)
-        } else {
-          return .just(ip)
-        }
-      }
+      client.run(command: "ip").stringFor(key: "ip")
     }
     
     public var containers: Containers {
