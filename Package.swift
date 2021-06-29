da83e5ccd4f01dfb9bd4471b43ebb0cb961c327f
Yury Korolev <yury.korolev@gmail.com>
2021-06-29 21:10:27 +0300

    Fix deps

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     ),
     .target(
       name: "Machines",
-      dependencies: ["Promise"]),
+      dependencies: ["Promise", "NonStdIO"]),
     .testTarget(
       name: "MachinesTests",
       dependencies: ["Machines", "NonStdIO"]),
