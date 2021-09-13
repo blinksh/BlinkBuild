90576edfec9c71d4322cb164e24934f265b6ebf1
Yury Korolev <yury.korolev@gmail.com>
2021-09-13 14:32:54 +0300

    Update swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     // .package(path: "../Spinner"),
     .package(url: "https://github.com/yury/Promise", from: "3.0.0"),
     .package(url: "https://github.com/yury/Spinner", from: "1.3.7"),
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0"))
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.5.0"))
   ],
   targets: [
     .target(
